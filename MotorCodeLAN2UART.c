c5601b2b81985fc857c7dc1eab7e1254ed78589a
Apoorv Thapliyal <57452076+Apoorv-1009@users.noreply.github.com>
2021-02-25 07:13:37 +0530

    Update MotorCodeLAN2UART.c

--- MotorCodeLAN2UART.c
+++ MotorCodeLAN2UART.c
@@ -1,14 +1,14 @@
 /*
- Code implements Motor Code on LEDs thru PWM with Timer4, by reading Joystick values
+  Code implements Motor Code on LEDs thru PWM with Timer4, by reading Joystick values
  from a phone via LAN2UART.
  Phone Joystick->Switcher->(LAN2UART)->STMF1
  Setup:
  PWM LEDs: PB6(Ch1), PB7(Ch2)
- UART: PA10(Rx)
+ UART: PA9(Tx), PA10(Rx)
        Left           Right
  LED:  PA4            PA5
  PWM:  PB6            PB7
- Same code was later run with our GUI, values sent from right joystick of the DS4
+ PB4 By default is HIGH, so we disable it in GPIO_Initialize()
  */
 
 #include "stm32f1xx.h"
@@ -46,8 +46,16 @@ void GPIO_Initialize()
 
 	//PA10 Setup: (UART Rx)
 	GPIOA->CRH &= ~(GPIO_CRH_MODE10);   //INPUT Mode (00)
-	GPIOA->CRH |= GPIO_CRH_CNF10_1;   //Input with pull-up/pull-down (10)
+	GPIOA->CRH |= GPIO_CRH_CNF10;   //Input with pull-up/pull-down (10)
 	GPIOA->CRH &= ~(GPIO_CRH_CNF10_0);
+
+	//PB4 Setup:
+	//Disable SWD & JTAG:
+	AFIO->MAPR |= AFIO_MAPR_SWJ_CFG_2;
+	AFIO->MAPR &= ~(AFIO_MAPR_SWJ_CFG_1 | AFIO_MAPR_SWJ_CFG_0);
+	GPIOB->CRL |= (GPIO_CRL_MODE4);   //OUTPUT Mode (11)
+	GPIOB->CRL &= ~(GPIO_CRL_CNF4);   //Output Push-Pull (00)
+	GPIOB->BRR = 1 << (4);   //Mandatory turn off
 }
 
 void Timer_Initialize()
@@ -81,8 +89,8 @@ void UART_Initilaize()
 	RCC->APB2ENR |= RCC_APB2ENR_USART1EN;   //UART1 Enable, Clk freq = 8Mhz
 	//Setting up Baud Rate:
 	USART1->BRR |= 4<<4 | 5<<0;   //Gives 115200 Baud Rate(approx.) Register Value = (8MHz)/(16 * Reqd. Baud Rate) = 4.5
-	//              Rx Enable      UART Enable
-	USART1->CR1 |= (USART_CR1_RE | USART_CR1_UE);
+	//              Rx Enable      Tx Enable	  UART Enable
+	USART1->CR1 |= (USART_CR1_RE | USART_CR1_TE | USART_CR1_UE);
 }
 
 uint8_t getuval()   //Reads UART Values
@@ -93,8 +101,7 @@ uint8_t getuval()   //Reads UART Values
 	return data;
 }
 
-//        |Left    Right| |      Left PWM       |  |      Right PWM     | |  Values  |  | Gear |
-void Drive(int DL, int DR, int oct0, int a, int b, int oct1, int p, int q,int X, int Y, float gear)
+void Drive(int DL, int DR, int a, int b, int p, int q, int X, int Y, float gear)
 {
 	if (DL == 1)
 		GPIOA->BSRR |= 1 << 4;   //Turn on LEFT LED
@@ -106,93 +113,55 @@ void Drive(int DL, int DR, int oct0, int a, int b, int oct1, int p, int q,int X,
 	else
 		GPIOA->BRR |= 1 << 5;   //Turn off RIGHT LED
 
-	TIM4->CCR1 = (uint32_t) (abs(8000 * oct0 - abs(X * a) - abs(Y * b)) * (gear*0.1));   //Left PWM
-	TIM4->CCR2 = (uint32_t) (abs(8000 * oct1 - abs(X * p) - abs(Y * q)) * (gear*0.1));   //Right PWM
+	TIM4->CCR1 = (uint32_t) abs(abs(a*X) - abs(b*Y))*(gear*0.1);   //Left PWM
+	TIM4->CCR2 = (uint32_t) abs(abs(p*X) - abs(q*Y))*(gear*0.1);   //Right PWM
 }
 
 void MotorCode(int x, int y, float g)
 {
-	if (abs(x) < 100 && abs(y) < 100)   //No Motion
-		Drive(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, g);
+//void Drive(int DL, int DR, int a, int b, int p, int q, int X, int Y, int g)
 
-	else if (abs(x) < 100 && y < 0)   //Full Forward
-		Drive(1, 1, 0, 0, 1, 0, 0, 1, x, y, g);
+	if (abs(x) < 20 && abs(y) < 20)   //No Motion
+		Drive(0,0,0,0,0,0,0,0,0);
 
-	else if (abs(x) < 100 && y > 0)   //Full Backward
-		Drive(0, 0, 0, 0, 1, 0, 0, 1, x, y, g);
+	else if(abs(x) < 10 && y < 0)   //Full Backward
+		Drive(0,0,0,1,0,1,x,y,g);
 
-	else if (x < 0 && abs(y) <= 100)   //Spot Turn Left
-		Drive(0, 1, 0, 1, 0, 0, 1, 0, x, y, g);
+	else if(abs(x) < 10 && y > 0)   //Full Forward
+		Drive(1,1,0,1,0,1,x,y,g);
 
-	else if (x > 0 && abs(y) <= 100)   //Spot Turn Right
-		Drive(1, 0, 0, 1, 0, 0, 1, 0, x, y, g);
+	else if (x < 0 && abs(y) <= 10)   //Spot Turn Left
+		Drive(0,1,1,0,1,0,x,y,g);
 
-	else if (x > 0 && y < 0 && x >= abs(y))   //Octet 1
-	{
-		if (abs(x) > 8000 - abs(y))
-			Drive(1, 0, 0, 1, 0, 1, 0, 1, x, y, g);
-		else
-			Drive(1, 0, 1, 0, 1, 0, 1, 0, x, y, g);
-	}
+	else if (x > 0 && abs(y) <= 10)   //Spot Turn Right
+		Drive(1,0,1,0,1,0,x,y,g);
 
-	else if (x > 0 && y < 0 && x < abs(y))   //Octet 2
-	{
-		if (abs(y) > 8000 - abs(x))
-			Drive(1, 1, 0, 0, 1, 1, 1, 0, x, y, g);
-		else
-			Drive(1, 1, 1, 1, 0, 0, 0, 1, x, y, g);
-	}
+	else if(x > 0 && y > 0 && x > y)   //Octet 1
+		Drive(1,0,1,0,1,1,x,y,g);
 
-	else if (x < 0 && y < 0 && abs(y) > abs(x))   //Octet 3
-	{
-		if (abs(y) > 8000 - abs(x))
-			Drive(1, 1, 1, 1, 0, 0, 0, 1, x, y, g);
-		else
-			Drive(1, 1, 0, 0, 1, 1, 1, 0, x, y, g);
-	}
+	else if(x > 0 && y > 0 && x < y)   //Octet 2
+		Drive(1,1,0,1,1,1,x,y,g);
 
-	else if (x < 0 && y < 0 && abs(x) >= abs(y))   //Octet 4
-	{
-		if (abs(x) > 8000 - abs(y))
-			Drive(0, 1, 1, 0, 1, 0, 1, 0, x, y, g);
-		else
-			Drive(0, 1, 0, 1, 0, 1, 0, 1, x, y, g);
-	}
+	else if(x < 0 && y > 0 && abs(x) < y)   //Octet 3
+		Drive(1,1,1,1,0,1,x,y,g);
 
-	else if (x < 0 && y > 0 && abs(x) > abs(y))   //Octet 5
-	{
-		if (abs(x) > 8000 - abs(y))
-			Drive(0, 1, 0, 1, 0, 1, 0, 1, x, y, g);
-		else
-			Drive(0, 1, 1, 0, 1, 0, 1, 0, x, y, g);
-	}
+	else if(x < 0 && y > 0 && abs(x) >= y)   //Octet 4
+		Drive(0,1,1,1,1,0,x,y,g);
 
-	else if (x < 0 && y > 0 && abs(y) >= abs(x))   //Octet 6
-	{
-		if (abs(y) > 8000 - abs(x))
-			Drive(0, 0, 0, 0, 1, 1, 1, 0, x, y, g);
-		else
-			Drive(0, 0, 1, 1, 0, 0, 0, 1, x, y, g);
-	}
+	else if(x < 0 && y < 0 && abs(x) > abs(y))   //Octet 5
+	 	Drive(0,1,1,0,1,1,x,y,g);
 
-	else if (x > 0 && y > 0 && abs(y) >= abs(x))   //Octet 7
-	{
-		if (abs(y) > 8000 - abs(x))
-			Drive(0, 0, 1, 1, 0, 0, 0, 1, x, y, g);
-		else
-			Drive(0, 0, 0, 0, 1, 1, 1, 0, x, y, g);
-	}
+	else if(x < 0 && y < 0 && abs(x) < abs(y))   //Octet 6
+	 	Drive(0,0,0,1,1,1,x,y,g);
 
-	else if (x > 0 && y > 0 && abs(x) > abs(y))   //Octet 8
-	{
-		if (abs(x) > 8000 - abs(y))
-			Drive(1, 0, 1, 0, 1, 0, 1, 0, x, y, g);
-		else
-			Drive(1, 0, 0, 1, 0, 1, 0, 1, x, y, g);
-	}
+	else if(x > 0 && y < 0 && abs(x) < abs(y))   //Octet 7
+	 	Drive(0,0,1,1,0,1,x,y,g);
+
+	else if(x > 0 && y < 0 && abs(x) > abs(y))   //Octet 8
+	 	Drive(0,1,1,0,1,1,x,y,g);
 
 	//Test Drive:
-	//Drive(1,1,0,1,0,0,0,1,x,y,g);
+	//Drive(1,1,1,0,0,1,x,y,g);
 }
 
 int main()
@@ -207,31 +176,33 @@ int main()
 	while (1)
 	{
 		//Read LAN2UART Values
-		if(getuval() == 'm')
+        	if(getuval() == 'm')
+        	{
+            		gear = (int) ((getuval() - '0') + 1);   //Get gear value
+            	if(getuval() == 's')
 		{
-			gear = (int) ((getuval() - '0') + 1);   //Get gear value
-			if(getuval() == 's')
-			{
-				x = (getuval()-'0')*10000 + (getuval()-'0')*1000 + (getuval()-'0')*100 + (getuval()-'0')*10 + (getuval()-'0');   //x value
-			}
-			if(getuval() == 'f')
-			{
-				y = (getuval()-'0')*10000 + (getuval()-'0')*1000 + (getuval()-'0')*100 + (getuval()-'0')*10 + (getuval()-'0');   //y value
-			}
-			trash = getuval();   //This is actually Mast CAM values but we're ignoring it for now
+                	x = (getuval()-'0')*10000 + (getuval()-'0')*1000 + (getuval()-'0')*100 + (getuval()-'0')*10 + (getuval()-'0');   //x value
 		}
-		else
+		if(getuval() == 'f')
 		{
-			trash = trash + 1 - 1;   //Bakchodi
-		 }
-		x = x - 8000;  //Adjust x-axis
-		y = 8000 - y;  //Adjust y-axis
-		if(abs(x) < 1000)
-			x = 0;
-		if(abs(y) < 1000)
-			y = 0;
-		MotorCode(x, y, gear);   //Run MotorCode
+                	y = (getuval()-'0')*10000 + (getuval()-'0')*1000 + (getuval()-'0')*100 + (getuval()-'0')*10 + (getuval()-'0');   //y value
+		}
+		trash = getuval();   //This is actually Mast CAM values but we're ignoring it for now
+        }
+	else
+	{
+		trash = trash + 1 - 1;   //Bakchodi
+	}
+        x = x - 8000;
+        y = y - 8000;
+
+        if(abs(x) < 500)
+        	x = 0;
+        if(abs(y) < 500)
+        	y = 0;
+
+        MotorCode(x, y, gear);   //Run MotorCode
+
 	}
 }
-
-
+/**/
